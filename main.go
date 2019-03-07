d2f57557cb8e21f958654b54587a50252eb67683
davishoang1993 <davis.hoang@4doutlets.com>
2019-03-06 20:53:22 -0600

    Amateur

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+
+)
+
+func main() {
+    power := 9000
+    fmt.Printf("It's over %d\n",power)
+    
+    power := 9001
+    fmt.Printf("It is also %d\n", power)
+}
